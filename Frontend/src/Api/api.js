bdc525aa1d23d45b038be075e991d068b7366375
unknown <challasandeep05@gmail.com>
2025-05-21 18:53:10 +0530

    file updated

--- Frontend/src/Api/api.js
+++ Frontend/src/Api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/api";
+const API_URL = "https://todo-full-stack-assignment.onrender.com/api";
 
 export const getTodos = () => axios.get(`${API_URL}/todos`);
 export const addTodo = (text) => axios.post(`${API_URL}/todos`, { text });
